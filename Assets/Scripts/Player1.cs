559ef550234e5b5d7b8c250e8c4886384df7624a
JCortes2178 <44535962+JCortes2178@users.noreply.github.com>
2024-09-22 17:01:48 -0400

    updates the neutral throw attack case statement to C#9

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -80,21 +80,21 @@ public void neutralThrowAttack()
     int List hurtbox_extended = [2, 3, 4, 5]; //TODO: ask if this is how to do the extension
 
     //play attack animation
-    switch state[1]
+    switch (s)tate[1])
     {
         //state[1] should be how many frames into the action the player is
         case 0:
             playAttackAnim();
 
-        case state[1] < (attackStartup - 1):
+        case < (attackStartup - 1):
             continue; 
         
-        case (attackStartup):
+        case attackStartup:
             if(hitbox.contains(Opponent.position) && Opponent.stance != "forward" && Opponent.stance != "backward")
             {   //Values are at the beginning of the function
                 Opponent.getHit(damage, hitstun, anim);
             }
-        case (state[1] < (attackStartup + attackRecovery)):
+        case < (attackStartup + attackRecovery)):
             continue;
         case (attackStartup + attackRecovery):
             state = ["actionable", 0];
