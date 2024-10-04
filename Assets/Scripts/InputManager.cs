bf29a3163970e8ca90a8e0248d5b32908ac86c53
KevinL135 <luokevin135@gmail.com>
2024-10-03 21:27:09 -0400

    fixed movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -18,7 +18,7 @@ public class InputManager : MonoBehaviour
         DontDestroyOnLoad(this);
     }
     public bool KeyDown(string key) {
-        if (Input.GetKeyDown(keybindings.checkKey(key))) {
+        if (Input.GetKey(keybindings.checkKey(key))) {
             return true;
         }
         else {
