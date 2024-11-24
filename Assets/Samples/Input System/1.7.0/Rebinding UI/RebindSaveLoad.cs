5ce3e5c7250b2ffbad1718e8c5b0ca98a907f398
JCortes2178 <44535962+JCortes2178@users.noreply.github.com>
2024-11-24 15:21:55 -0500

    updates the player input screens to two seperate screens

--- Assets/Samples/Input System/1.7.0/Rebinding UI/RebindSaveLoad.cs
+++ Assets/Samples/Input System/1.7.0/Rebinding UI/RebindSaveLoad.cs
@@ -7,6 +7,10 @@ public class RebindSaveLoad : MonoBehaviour
 
     public void OnEnable()
     {
+        Debug.Log("I AM IN THE ENABLE FUNCTION");
+        Debug.Log("text1e");
+        Debug.Log(this.name);
+        Debug.Log("text2e");
         var rebinds = PlayerPrefs.GetString("rebinds");
         if (!string.IsNullOrEmpty(rebinds))
             actions.LoadBindingOverridesFromJson(rebinds);
@@ -14,6 +18,10 @@ public class RebindSaveLoad : MonoBehaviour
 
     public void OnDisable()
     {
+        Debug.Log("I AM IN THE DISABLE FUNCTION", this);
+        Debug.Log("text1d");
+        Debug.Log(this.name);
+        Debug.Log("text2d");
         var rebinds = actions.SaveBindingOverridesAsJson();
         PlayerPrefs.SetString("rebinds", rebinds);
     }
